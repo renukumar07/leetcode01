2104f8a22915c77a2ea01f62b1edbfff32013649
renukumar07 <renukumar90@gmail.com>
2022-12-14 09:20:57 +0530

    Time: 2 ms (98.83%), Space: 40.5 MB (93.39%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,11 +1,16 @@
 class Solution {
+    private boolean isVowel(char c){
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' ||c=='I' || c=='O' || c=='U')
+            return true;
+        return false;
+    }
     public boolean halvesAreAlike(String s) {
-        var vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
-        int a = 0, b = 0;
-        for (int i = 0, j = s.length() - 1; i < j; ++i, --j) {
-            a += vowels.contains(s.charAt(i)) ? 1 : 0;
-            b += vowels.contains(s.charAt(j)) ? 1 : 0;
+        int leftCount = 0;
+        int rightCount = 0;
+        for(int i = 0,j = s.length()/2;i<s.length()/2;i++,j++){
+            if(isVowel(s.charAt(i))) leftCount++;
+            if(isVowel(s.charAt(j))) rightCount++;
         }
-        return a == b;
+        return leftCount==rightCount;
     }
 }
