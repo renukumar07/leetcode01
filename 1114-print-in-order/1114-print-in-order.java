f14185243bbeb67c894968f824c6b70f302825ca
renukumar07 <renukumar90@gmail.com>
2023-04-07 08:40:33 +0530

    Time: 13 ms (32.66%), Space: 41.7 MB (49.93%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,30 @@
+class Foo {
+
+    public Foo() {
+        
+    }
+
+    Semaphore firstSemaphore = new Semaphore(1);
+    Semaphore secondSemaphore = new Semaphore(0);
+    Semaphore thirdSemaphore = new Semaphore(0);
+    public void first(Runnable printFirst) throws InterruptedException {
+        firstSemaphore.acquire();
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        secondSemaphore.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        secondSemaphore.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        thirdSemaphore.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        thirdSemaphore.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+        firstSemaphore.release();
+    }
+}
