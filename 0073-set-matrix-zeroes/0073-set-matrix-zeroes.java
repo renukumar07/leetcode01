a6de3469c77b088bcc986394fb7aecdd93e75e0d
renukumar07 <renukumar90@gmail.com>
2023-06-07 20:00:21 +0530

    Time: 1 ms (72.55%), Space: 44.6 MB (30.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,29 +1,23 @@
 class Solution {
-    class XY{
-        int x;
-        int y;
-        XY(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
     public void setZeroes(int[][] matrix) {
-        List<XY> xyList = new ArrayList<>();
+        Set<Integer> xSet = new HashSet<>();
+        Set<Integer> ySet = new HashSet<>();        
         int m = matrix.length;
         int n = matrix[0].length;
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(matrix[i][j]==0){
-                    xyList.add(new XY(i, j));
+                    xSet.add(i);
+                    ySet.add(j);
                 }
             }
         }
-        for(XY xy: xyList){
-            int x = xy.x;
-            int y = xy.y;
+        for(int x: xSet){
             for(int i = 0;i<n;i++){
                 matrix[x][i] = 0;
             }
+        }
+        for(int y: ySet){
             for(int i = 0;i<m;i++){
                 matrix[i][y] = 0;
             }
