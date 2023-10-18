cc6612160eab924ddc72414e3b1e0232373c7d8e
Renu Kumar Chebrolu <renukumar90@gmail.com>
2023-10-18 17:48:45 +0530

    Time: 729 ms (81.74%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-
-SELECT p.firstName, p.lastName, a.city, a.state 
-from Person p
-LEFT JOIN Address a 
-ON p.personId = a.personId;
+select p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a
+on p.personId = a.personId; 
