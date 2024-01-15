31994e0ba1d609841aeebae4bf645ce4c79fc50a
Renu Kumar Chebrolu <renukumar90@gmail.com>
2024-01-15 21:55:57 +0530

    Time: 100 ms (31.09%), Space: 86.8 MB (88.87%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,14 +1,47 @@
 class Solution {
-    public List<List<Integer>> findWinners(int[][] matches){
-        Map<Integer, Integer> losses = new TreeMap<>();
-        for(int[] m : matches){
-            losses.put(m[0], losses.getOrDefault(m[0], 0));
-            losses.put(m[1], losses.getOrDefault(m[1], 0) + 1);
+    public List<List<Integer>> findWinners(int[][] matches) {
+        Set<Integer> winnerSet = new HashSet<>();
+        Map<Integer, Integer> loserMap = new HashMap<>();
+        
+        for(int i = 0;i<matches.length;i++){
+            int winner = matches[i][0];
+            int loser = matches[i][1];
+            winnerSet.add(winner);   
+            
+            loserMap.put(loser, loserMap.getOrDefault(loser, 0)+1);
+                        
         }
-        List<List<Integer>> r = Arrays.asList(new ArrayList<>(), new ArrayList<>());
-        for(Integer player : losses.keySet())
-            if(losses.get(player) <= 1)
-                r.get(losses.get(player)).add(player);
-        return r;
+        
+        for(int i = 0;i<matches.length;i++){
+
+            int loser = matches[i][1];
+
+            if(winnerSet.contains(loser)){
+                winnerSet.remove(loser);
+            }
+
+        }
+        
+        
+        List<Integer> list1 = new ArrayList<>();
+        
+        for(int a: winnerSet){
+            list1.add(a);
+        }
+        
+        List<Integer> list2 = new ArrayList<>();
+        
+        for(Map.Entry<Integer, Integer> entry: loserMap.entrySet()){
+            if(entry.getValue()==1){
+                list2.add(entry.getKey());
+            }
+        }
+        Collections.sort(list1);
+        Collections.sort(list2);
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(list1);
+        ans.add(list2);
+        return ans;
+        
     }
 }
